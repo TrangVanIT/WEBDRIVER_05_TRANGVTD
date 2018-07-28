940faf49e3d6a0463848b34e52fbe89d0cba5406
TrangVanIT <doantrang2408@gmail.com>
2018-07-28 17:06:41 +0700

    Topic 02 exercise

---
+++ src/java_01_basic/test.java
@@ -0,0 +1,9 @@
+package java_01_basic;
+
+import org.testng.annotations.Test;
+
+public class test {
+  @Test
+  public void f() {
+  }
+}
--- src/selenium_api/Topic_04_WebElement_WebBrowserCommands.java
+++ src/selenium_api/Topic_04_WebElement_WebBrowserCommands.java
@@ -1,4 +1,4 @@
-package WebElement_WebBrowserCommands;
+package selenium_api;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
